07c28d43cf93375833d5c8870499f873cc7716d6
sakuramochi <sakuramochi.mochi.0@gmail.com>
2017-07-28 14:36:34 +0900

    add latest updated time

--- mirror.py
+++ mirror.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import datetime
 import requests
 from bs4 import BeautifulSoup
 
@@ -7,7 +8,11 @@ def midland():
     url = 'http://www.midland-sq-cinema.jp/'
     r = requests.get(url)
     s = BeautifulSoup(r.text, 'lxml')
-    html = s.select('#topics_area')[0].prettify()
+    mtime = datetime.datetime.now()
+    time_str = mtime.strftime('%Y/%m/%d %H:%M')
+    html = '<p>最終更新日時: {}</p>\n'.format(time_str)
+    html += s.select('#topics_area')[0].prettify()
+    
     with open('html/midland.html', 'w') as f:
         f.write(html)
 
